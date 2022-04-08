fde99b384c7d2c25dd1f83f962f06306d0e6c720
yousiftalaq <yousiftalaq1998@gmail.com>
2022-04-09 01:17:35 +0300

    End Section 4

---
+++ Yousif_Models/Models/ViewModels/InquiryVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yousif_Models.Models.ViewModels
+{
+    public class InquiryVM
+    {
+
+        public InquiryHeader InquiryHeader { get; set; }
+
+        public IEnumerable<InquiryDetail> InquiryDetail { get; set; }   
+
+    }
+}
---
+++ Yousif_Project/Controllers/InquiryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Yousif_DataAccess.Repository.IRepository;
+
+namespace Yousif_Project.Controllers
+{
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inqHRepo;
+
+        private readonly IInquiryDetailRepository _inqDRepo;
+
+        public InquiryController(IInquiryHeaderRepository inqHRepo, IInquiryDetailRepository inqDRepo)
+        {
+            _inqHRepo = inqHRepo;
+            _inqDRepo = inqDRepo;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
